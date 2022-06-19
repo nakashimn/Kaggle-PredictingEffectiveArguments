00487b7583d16ffa5e21cb8bf5b01c76dc6ee537
nakashimn <nakashimn@gmail.com>
2022-06-19 23:39:09 +0900

    modify:fix_bug_on_criterion

--- src/baseline.py
+++ src/baseline.py
@@ -24,7 +24,7 @@ from transformers import AutoTokenizer, AutoModel
 config = {
     "mode": "train",
     "epoch": 100,
-    "n_splits": 5,
+    "n_splits": 3,
     "random_seed": 57,
     "label": "discourse_effectiveness",
     "experiment_name": "roberta-v0",
@@ -139,37 +139,37 @@ class TextCleaner:
         # Replace Upper to Lower
         data[col] = data[col].str.lower()
         # Replace
-        data[col] = data[col].str.replace(r"what's", "what is ")
-        data[col] = data[col].str.replace(r"\'ve", " have ")
-        data[col] = data[col].str.replace(r"can't", "cannot ")
-        data[col] = data[col].str.replace(r"n't", " not ")
-        data[col] = data[col].str.replace(r"i'm", "i am ")
-        data[col] = data[col].str.replace(r"\'re", " are ")
-        data[col] = data[col].str.replace(r"\'d", " would ")
-        data[col] = data[col].str.replace(r"\'ll", " will ")
-        data[col] = data[col].str.replace(r"\'scuse", " excuse ")
-        data[col] = data[col].str.replace(r"\'s", " ")
+        data[col] = data[col].str.replace(r"what's", "what is ", regex=True)
+        data[col] = data[col].str.replace(r"\'ve", " have ", regex=True)
+        data[col] = data[col].str.replace(r"can't", "cannot ", regex=True)
+        data[col] = data[col].str.replace(r"n't", " not ", regex=True)
+        data[col] = data[col].str.replace(r"i'm", "i am ", regex=True)
+        data[col] = data[col].str.replace(r"\'re", " are ", regex=True)
+        data[col] = data[col].str.replace(r"\'d", " would ", regex=True)
+        data[col] = data[col].str.replace(r"\'ll", " will ", regex=True)
+        data[col] = data[col].str.replace(r"\'scuse", " excuse ", regex=True)
+        data[col] = data[col].str.replace(r"\'s", " ", regex=True)
         # Remove
-        data[col] = data[col].str.replace(r'\s', ' ')
-        data[col] = data[col].str.replace('.', ' ')
-        data[col] = data[col].str.replace(',', ' ')
-        data[col] = data[col].str.replace('\"', ' ')
-        data[col] = data[col].str.replace('(', ' ')
-        data[col] = data[col].str.replace(')', ' ')
-        data[col] = data[col].str.replace(':', ' ')
-        data[col] = data[col].str.replace(';', ' ')
+        data[col] = data[col].str.replace(r'\s', ' ', regex=True)
+        data[col] = data[col].str.replace('.', ' ', regex=True)
+        data[col] = data[col].str.replace(',', ' ', regex=True)
+        data[col] = data[col].str.replace('\"', ' ', regex=True)
+        data[col] = data[col].str.replace('(', ' ', regex=True)
+        data[col] = data[col].str.replace(')', ' ', regex=True)
+        data[col] = data[col].str.replace(':', ' ', regex=True)
+        data[col] = data[col].str.replace(';', ' ', regex=True)
         # Clean some punctutations
-        data[col] = data[col].str.replace(r'([a-zA-Z]+)([/!?.])([a-zA-Z]+)',r'\1 \2 \3')
+        data[col] = data[col].str.replace(r'([a-zA-Z]+)([/!?.])([a-zA-Z]+)',r'\1 \2 \3', regex=True)
         # Replace repeating characters more than 3 times to length of 3
-        data[col] = data[col].str.replace(r'([*!?\'])\1\1{2,}',r'\1\1\1')
+        data[col] = data[col].str.replace(r'([*!?\'])\1\1{2,}',r'\1\1\1', regex=True)
         # Add space around repeating characters
-        data[col] = data[col].str.replace(r'([*!?\']+)',r' \1 ')
+        data[col] = data[col].str.replace(r'([*!?\']+)',r' \1 ', regex=True)
         # patterns with repeating characters
-        data[col] = data[col].str.replace(r'([a-zA-Z])\1{2,}\b',r'\1\1')
-        data[col] = data[col].str.replace(r'([a-zA-Z])\1\1{2,}\B',r'\1\1\1')
-        data[col] = data[col].str.replace(r'[ ]{2,}',' ').str.strip()
-        data[col] = data[col].str.replace(r'[ ]{2,}',' ').str.strip()
-        data[col] = data[col].str.replace(r' +', ' ')
+        data[col] = data[col].str.replace(r'([a-zA-Z])\1{2,}\b',r'\1\1', regex=True)
+        data[col] = data[col].str.replace(r'([a-zA-Z])\1\1{2,}\B',r'\1\1\1', regex=True)
+        data[col] = data[col].str.replace(r'[ ]{2,}',' ', regex=True).str.strip()
+        data[col] = data[col].str.replace(r'[ ]{2,}',' ', regex=True).str.strip()
+        data[col] = data[col].str.replace(r' +', ' ', regex=True)
         # data[col] = data[col].apply(lambda x: ' '.join([word for word in x.split() if word not in (self.stop)]))
         return data
 
@@ -197,8 +197,8 @@ class PeDataset(Dataset):
         if self.transform is not None:
             ids = self.transform(ids)
         if self.labels is not None:
-            label = self.labels[idx]
-            return ids, masks, label
+            labels = self.labels[idx]
+            return ids, masks, labels
         return ids, masks
 
     def tokenize(self, text):
@@ -282,36 +282,38 @@ class PeModel(LightningModule):
         ids, masks, labels = batch
         logits = self.forward(ids, masks)
         loss = self.criterion(logits, labels)
-        prob = logits.sigmoid().detach().cpu()
-        label = labels.detach().cpu()
-        return {"loss": loss, "prob": prob, "label": label}
+        logit = logits.detach()
+        label = labels.detach()
+        return {"loss": loss, "logit": logit, "label": label}
 
     def validation_step(self, batch, batch_idx):
         ids, masks, labels = batch
         logits = self.forward(ids, masks)
         loss = self.criterion(logits, labels)
-        prob = logits.sigmoid().detach().cpu()
-        label = labels.detach().cpu()
-        return {"loss": loss, "prob": prob, "label": label}
+        logit = logits.detach()
+        prob = logits.sigmoid().detach()
+        label = labels.detach()
+        return {"loss": loss, "logit": logit, "prob": prob, "label": label}
 
     def predict_step(self, batch, batch_idx):
         ids, masks = batch
         logits = self.forward(ids, masks)
-        prob = logits.sigmoid().detach().cpu()
+        prob = logits.sigmoid().detach()
         return {"prob": prob}
 
     def training_epoch_end(self, outputs):
-        probs = torch.cat([out["prob"] for out in outputs])
+        logits = torch.cat([out["logit"] for out in outputs])
         labels = torch.cat([out["label"] for out in outputs])
-        metrics = self.criterion(probs, labels)
+        metrics = self.criterion(logits, labels)
         self.min_loss = np.nanmin([self.min_loss, metrics.detach().cpu().numpy()])
         self.log(f"train_loss", metrics)
         return super().training_epoch_end(outputs)
 
     def validation_epoch_end(self, outputs):
+        logits = torch.cat([out["logit"] for out in outputs])
         probs = torch.cat([out["prob"] for out in outputs])
         labels = torch.cat([out["label"] for out in outputs])
-        metrics = self.criterion(probs, labels)
+        metrics = self.criterion(logits, labels)
         self.log(f'val_loss', metrics)
 
         self.val_probs = probs.detach().cpu().numpy()
