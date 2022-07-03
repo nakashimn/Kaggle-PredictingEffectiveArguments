62a40dcc186a3551fa13e78c9ac7cb5557cb2b71
nakashimn <nakashimn@gmail.com>
2022-07-03 22:47:52 +0900

    add:distilbert

---
+++ src/distilbert.py
@@ -0,0 +1,182 @@
+from baseline import *
+
+config["mode"] = "train"
+config["n_splits"] = 3
+config["experiment_name"] = "fp-distilbert-v0"
+config["path"]["model_dir"] = "/kaggle/input/model/fp-distilbert-v0/"
+config["pred_ensemble"] = True
+config["model"] = {
+    "base_model_name": "/kaggle/input/distilbertbaseuncased",
+    "dim_feature": 768,
+    "num_class": 3,
+    "freeze_base_model": False,
+    "optimizer":{
+        "name": "optim.RAdam",
+        "params":{
+            "lr": 1e-5
+        },
+    },
+    "scheduler":{
+        "name": "optim.lr_scheduler.CosineAnnealingWarmRestarts",
+        "params":{
+            "T_0": 20,
+            "eta_min": 1e-4,
+        }
+    }
+}
+config["earlystopping"] = {
+    'patience': 3
+}
+config["checkpoint"] = {
+    "dirpath": config["path"]["temporal_dir"],
+    "monitor": "val_loss",
+    "save_top_k": 1,
+    "mode": "min",
+    "save_last": False,
+    "save_weights_only": False
+}
+config["trainer"] = {
+    "accelerator": "gpu",
+    "devices": 1,
+    "max_epochs": 100,
+    "accumulate_grad_batches": 1,
+    "fast_dev_run": False,
+    "num_sanity_val_steps": 0,
+    "resume_from_checkpoint": None,
+    "precision": 16
+}
+config["datamodule"] = {
+    "dataset":{
+        "base_model_name": config["model"]["base_model_name"],
+        "num_class": config["model"]["num_class"],
+        "label": config["label"],
+        "max_length": 512,
+        "discourse_effectiveness": {l : i for i, l in enumerate(config["labels"])},
+        "discourse_type": {tp : i for i, tp in enumerate(config["types"])}
+    },
+    "train_loader": {
+        "batch_size": 16,
+        "shuffle": True,
+        "num_workers": 16,
+        "pin_memory": True,
+        "drop_last": True,
+    },
+    "val_loader": {
+        "batch_size": 16,
+        "shuffle": False,
+        "num_workers": 16,
+        "pin_memory": True,
+        "drop_last": False
+    },
+    "pred_loader": {
+        "batch_size": 16,
+        "shuffle": False,
+        "num_workers": 16,
+        "pin_memory": False,
+        "drop_last": False
+    }
+}
+
+if __name__=="__main__":
+
+    # preprocessor
+    text_cleaner = TextCleaner()
+
+    if config["mode"]=="train":
+
+        # logger
+        mlflow_logger = create_mlflow_logger(config)
+
+        # Setting Dataset
+        df_train = pd.read_csv(config["path"]["traindata"])
+        df_train["essay"] = df_train["essay_id"].apply(fetchEssay, args=(config["path"]["trainessay"],))
+        df_train = text_cleaner.clean(df_train, "discourse_text")
+        df_train = text_cleaner.clean(df_train, "essay")
+
+        # Training
+        trainer = Trainer(
+            PeModel,
+            PeDataModule,
+            PeDataset,
+            AutoTokenizer,
+            ValidResult,
+            MinLoss,
+            df_train,
+            config,
+            None,
+            mlflow_logger
+        )
+        trainer.run()
+
+        # Validation Result
+        confmat = ConfusionMatrix(
+            trainer.val_probs.values,
+            trainer.val_labels.values,
+            config["Metrics"]
+        )
+        fig_confmat = confmat.draw()
+        fig_confmat.savefig(f"{config['path']['temporal_dir']}/confmat.png")
+        mlflow_logger.experiment.log_artifact(
+            mlflow_logger.run_id,
+            f"{config['path']['temporal_dir']}/confmat.png"
+        )
+
+        f1_score = F1Score(
+            trainer.val_probs.values,
+            trainer.val_labels.values,
+            config["Metrics"]
+        )
+        f1_scores = f1_score.calc()
+        mlflow_logger.log_metrics({
+            "macro_f1_score": f1_scores["macro"],
+            "micro_f1_score": f1_scores["micro"]
+        })
+
+        logloss = LogLoss(
+            trainer.val_probs.values,
+            trainer.val_labels.values
+        )
+        log_loss = loggloss.calc()
+        mlflow_logger.log_metrics({
+            "logloss": log_loss
+        })
+
+        # output
+        print(f"macro_f1_score: {f1_scores['macro']:.04f}")
+        print(f"micro_f1_score: {f1_scores['micro']:.04f}")
+        print(f"logloss: {log_loss:.04f}")
+
+        # update model
+        update_model(config)
+
+
+    if config["mode"]=="test":
+
+        # Setting Dataset
+        df_test = pd.read_csv(config["path"]["testdata"])
+        df_test["essay"] = df_test["essay_id"].apply(fetchEssay, args=(config["path"]["testessay"],))
+        df_test = text_cleaner.clean(df_test, "discourse_text")
+        df_test = text_cleaner.clean(df_test, "essay")
+
+        # Prediction
+        if config["pred_ensemble"]:
+            cls_predictor = PredictorEnsemble
+        else:
+            cls_predictor = Predictor
+        predictor = cls_predictor(
+            PeModel,
+            PeDataModule,
+            PeDataset,
+            AutoTokenizer,
+            df_test,
+            config,
+            None
+        )
+        predictor.run()
+
+        # output
+        submission = pd.concat([
+            df_test["discourse_id"],
+            pd.DataFrame(predictor.probs, columns=config["labels"])
+        ], axis=1)
+        submission.to_csv("submission.csv", index=None)
