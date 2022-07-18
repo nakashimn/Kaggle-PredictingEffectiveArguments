833b509f3e4f6113179165096f804bc0111b1771
nakashimn <nakashimn@gmail.com>
2022-07-18 21:47:01 +0900

    modify:update_dataset

--- src/components/datamodule.py
+++ src/components/datamodule.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader, Dataset
 from pytorch_lightning import LightningDataModule
 import traceback
 
-class FpDatasetTokenized(Dataset):
+class FpDataset(Dataset):
     def __init__(self, df, config, Tokenizer, transform=None):
         self.config = config
         self.tokenizer = Tokenizer.from_pretrained(
@@ -63,58 +63,6 @@ class FpDatasetTokenized(Dataset):
             padding="max_length"
         )
         return token
-
-class FpDataset(Dataset):
-    def __init__(self, df, config, Tokenizer, transform=None):
-        self.config = config
-        self.val = self.read_values(df)
-        self.labels = None
-        if self.config["label"] in df.keys():
-            self.labels = self.read_labels(df)
-        self.tokenizer = Tokenizer.from_pretrained(
-            self.config["base_model_name"],
-            use_fast=self.config["use_fast_tokenizer"]
-        )
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.val)
-
-    def __getitem__(self, idx):
-        ids, masks = self.tokenize(self.val[idx])
-        if self.transform is not None:
-            ids = self.transform(ids)
-        if self.labels is not None:
-            labels = self.labels[idx]
-            return ids, masks, labels
-        return ids, masks
-
-    def read_values(self, df):
-        values = (
-            df["discourse_type"]+ " " + df["discourse_text"] + " " + df["essay"]
-        ).values
-        return values
-
-    def read_labels(self, df):
-        labels = F.one_hot(
-            torch.tensor(
-                [self.config[self.config["label"]][d] for d in df[self.config["label"]]]
-            ),
-            num_classes=self.config["num_class"]
-        ).float()
-        return labels
-
-    def tokenize(self, text):
-        token = self.tokenizer.encode_plus(
-            text,
-            truncation=True,
-            add_special_tokens=True,
-            max_length=self.config["max_length"],
-            padding="max_length"
-        )
-        ids = torch.tensor(token["input_ids"], dtype=torch.long)
-        masks = torch.tensor(token["attention_mask"], dtype=torch.long)
-        return ids, masks
 
 class FpDataModule(LightningDataModule):
     def __init__(
